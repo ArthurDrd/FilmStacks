00ecdd8ca264dcabab0701800ac94caafc244367
ArthurDrd <arthurdurand.pro1@gmail.com>
2023-03-23 18:40:34 -0400

    tes

--- FilmStacks/app/src/main/java/com/example/filmstacks/MyApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.filmstacks
-
-import android.app.Application
-import com.example.filmstacks.data.MyDatabase
-
-class MyApplication : Application() {
-    val database: MyDatabase by lazy { MyDatabase.getDatabase(this) }
-}
-
