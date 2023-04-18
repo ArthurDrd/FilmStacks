ef911a0b8d2286c24a8f71fb747a5427d3b1d198
ArthurDrd <arthurdurand.pro1@gmail.com>
2023-04-17 22:44:40 -0400

    Update MovieEntity.kt

--- FilmStacks/app/src/main/java/com/example/filmstacks/data/local/MovieEntity.kt
+++ FilmStacks/app/src/main/java/com/example/filmstacks/data/local/MovieEntity.kt
@@ -1,27 +1,27 @@
-    package com.example.filmstacks.data.local
+package com.example.filmstacks.data.local
 
-    import androidx.room.Entity
-    import androidx.room.PrimaryKey
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-    @Entity
-    data class MovieEntity(
-        @PrimaryKey val id: Int,
-        val title: String,
-        val overview: String,
-        val adult: Boolean?,
-        val backdrop_path: String?,
-        val poster_path: String?,
-        val release_date: String?,
-        val vote_average: Double?,
-        val budget: Int?,
-        val popularity: Double?,
-        val runtime: Int?,
-        val status: String?,
-        val genres: List<GenreEntity>? = null
-    )
+@Entity
+data class MovieEntity(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val overview: String,
+    val adult: Boolean?,
+    val backdrop_path: String?,
+    val poster_path: String?,
+    val release_date: String?,
+    val vote_average: Double?,
+    val budget: Int?,
+    val popularity: Double?,
+    val runtime: Int?,
+    val status: String?,
+    val genres: List<GenreEntity>? = null
+)
 
-    @Entity
-    data class GenreEntity(
-        @PrimaryKey val id: Int,
-        val name: String
-    )
+@Entity
+data class GenreEntity(
+    @PrimaryKey val id: Int,
+    val name: String
+)
